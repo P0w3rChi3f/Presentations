bbce31b1db2082b2072ed1f6ba8b8dafafc89ab0
James Honeycutt <honeycutt.james@gmail.com>
2020-02-11 21:03:24 -0500

    Removed PowerShell Crash Course and PowerShell JEA

--- PowerShell/PowerShell Crash Course/DemoScripts/Get-Comparison.ps1
+++
@@ -1,9 +0,0 @@
-﻿get-service | where-object -Filter {$_.status -eq 'running’ }
-
-Get-Service | Where-Object Status -eq 'Running’
-    
-Get-Service | Where-Object {$_.status -eq 'running' -AND $_.StartType -eq 'Manual’}
-
-Get-Process | Where-Object -Filter { $_.Name -notlike 'powershell*' } | 
-    Sort VM -descending | Select -first 10 -Property ProcessName, VM, ID | 
-    Measure-Object -property VM -sum
