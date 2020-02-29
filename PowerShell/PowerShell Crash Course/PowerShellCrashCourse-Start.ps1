8029164dd89296da67fc462121d99b216763b2d9
James Honeycutt <honeycutt.james@gmail.com>
2020-02-28 19:40:25 -0500

    More MAC changes

--- PowerShell/PowerShell Crash Course/PowerShellCrashCourse-Start.ps1
+++ PowerShell/PowerShell Crash Course/PowerShellCrashCourse-Start.ps1
@@ -1,60 +1,60 @@
-﻿New-Item -Path c:/ -ItemType Directory -Name Demo
-Set-Location C:/Demo
+﻿New-Item -Path .. -ItemType Directory -Name Demo
+Set-Location /Demo
 #Discover Commands
-code './PowerShell/PowerShell Crash Course/DemoScripts/Discovering Commands.ps1'
+code '../PowerShell/PowerShell Crash Course/DemoScripts/Discovering Commands.ps1'
 
 #Navigating the File System
-code './PowerShell/PowerShell Crash Course/DemoScripts/Get-FileNavigation.ps1'
+code '../PowerShell/PowerShell Crash Course/DemoScripts/Get-FileNavigation.ps1'
 
 #Object Demo
-code './PowerShell/PowerShell Crash Course/DemoScripts/get-ObjectDemo.ps1'
+code '../PowerShell/PowerShell Crash Course/DemoScripts/get-ObjectDemo.ps1'
 
 #variables in Quotes
-code './PowerShell/PowerShell Crash Course/DemoScripts/Variables-inQotes.ps1'
+code '../PowerShell/PowerShell Crash Course/DemoScripts/Variables-inQotes.ps1'
 
 #The PipeLine
-code './PowerShell/PowerShell Crash Course/DemoScripts/Pipline-Demo.ps1'
+code '../PowerShell/PowerShell Crash Course/DemoScripts/Pipline-Demo.ps1'
 
 #Logic Demo
-code './PowerShell/PowerShell Crash Course/DemoScripts/LogicDemo.ps1'
+code '../PowerShell/PowerShell Crash Course/DemoScripts/LogicDemo.ps1'
 
 #Adding Commands
-code './PowerShell/PowerShell Crash Course/DemoScripts/AddingCommands-Demo.ps1'
+code '../PowerShell/PowerShell Crash Course/DemoScripts/AddingCommands-Demo.ps1'
 
 #Commands from internet
-& "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe" www.powershellgallery.com
-code './PowerShell/PowerShell Crash Course/DemoScripts/Import-Commands.ps1'
+& "C:/Program Files (x86)/Google/Chrome/Application/chrome..exe" www..powershellgallery..com
+code '../PowerShell/PowerShell Crash Course/DemoScripts/Import-Commands.ps1'
 
 #Output Demo
-code './PowerShell/PowerShell Crash Course/DemoScripts/Output-Formating.ps1'
+code '../PowerShell/PowerShell Crash Course/DemoScripts/Output-Formating.ps1'
 
 #Input Demo
-code './PowerShell/PowerShell Crash Course/DemoScripts/Input-data.ps1'
+code '../PowerShell/PowerShell Crash Course/DemoScripts/Input-data.ps1'
 
 #Comparison Demo
-code './PowerShell/PowerShell Crash Course/DemoScripts/Get-Comparison.ps1'
+code '../PowerShell/PowerShell Crash Course/DemoScripts/Get-Comparison.ps1'
 
 #Remoting Demo
-code './PowerShell/PowerShell Crash Course/DemoScripts/Remote-Demo.ps1'
+code '../PowerShell/PowerShell Crash Course/DemoScripts/Remote-Demo.ps1'
 
 #WMI Demo
-code './PowerShell/PowerShell Crash Course/DemoScripts/WMI-Demo.ps1'
+code '../PowerShell/PowerShell Crash Course/DemoScripts/WMI-Demo.ps1'
 
 #Jobs Demo
-code './PowerShell/PowerShell Crash Course/DemoScripts/Jobs-Demo.ps1'
+code '../PowerShell/PowerShell Crash Course/DemoScripts/Jobs-Demo.ps1'
 
 #Logging Demo
-code './PowerShell/PowerShell Crash Course/DemoScripts/Logging-Demo.ps1'
+code '../PowerShell/PowerShell Crash Course/DemoScripts/Logging-Demo.ps1'
 
 #Scope
-code './PowerShell/PowerShell Crash Course/DemoScripts/Scope.ps1'
+code '../PowerShell/PowerShell Crash Course/DemoScripts/Scope.ps1'
 
 #Scripting Demo
-code './PowerShell/PowerShell Crash Course/DemoScripts/Scripting-Demo.ps1'
-code './PowerShell/PowerShell Crash Course/DemoScripts/Get-DiskInventory.ps1'
+code '../PowerShell/PowerShell Crash Course/DemoScripts/Scripting-Demo.ps1'
+code '../PowerShell/PowerShell Crash Course/DemoScripts/Get-DiskInventory.ps1'
 
 #Tips Demo
-code "./PowerShell/PowerShell Crash Course/DemoScripts/Tips-Demo.ps1"
+code "../PowerShell/PowerShell Crash Course/DemoScripts/Tips-Demo.ps1"
 
 
 
